237526de329a8d84366b2ee96234bb3f6bef21a4
Christopher Estanislau <christopher.estanis@gmail.com>
2020-02-23 21:06:09 -0300

    Add items to cart with plus and minus

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,9 +2,10 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess, updateAmount } from './action';
+import { addToCartSuccess, updateAmountSuccess } from './action';
 
 function* addToCart({ id }) {
   const productExists = yield select(state =>
@@ -14,17 +15,17 @@ function* addToCart({ id }) {
   const stock = yield call(api.get, `/stock/${id}`);
 
   const stockAmount = stock.data.amount;
-  const currentAmount =  productExists ? productExists.amount : 0;
+  const currentAmount = productExists ? productExists.amount : 0;
 
   const amount = currentAmount + 1;
-  
+
   if (amount > stockAmount) {
-    toast.error('Quantidade solicitada fora do estoque.')
+    toast.error('Quantidade solicitada fora do estoque.');
     return;
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -35,7 +36,24 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora do estoque.');
+    return;
+  }
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
